f9310edaf7e657ce90bd4313259733e0f292caa3
Ibrahim Alao <alaoopeyemi101@gmail.com>
2020-11-15 19:25:43 +0100

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config() 
 const morgan = require("morgan");
+const cors = require('cors')
 const connectDB = require("./config/db");
 const router = require('./routes/api/user');
 
@@ -13,6 +14,7 @@ connectDB();
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 
 //morgan logging in dev
